fd649e727417e0964748850a5e7bc2af3353c840
ap-lodhi <anilpatel.bcs@gmail.com>
2022-06-29 17:44:35 +0530

    top carousel

---
+++ src/Components/HomePage/StanzaSpeak.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Slider from "react-slick";
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import "./StanzaSpeak.css"
+import data from '../../data';
+
+
+const StanzaSpeak=()=>{
+    return(
+        
+        <div  className='top'>
+            <h1 className='heading'>#StanzaSpeak</h1>
+
+       <Slider autoplay={false}  
+       dots={true} 
+       slidesToShow={3} 
+      
+       >
+        
+        {data.map((item)=>(
+
+            <div className='slider'>
+                <img src={item} alt="loading " className='img'/>
+            </div>
+        ))}
+           
+       </Slider>
+
+        </div>
+        
+    )
+}
+export default StanzaSpeak;
---
+++ src/data.js
@@ -0,0 +1,10 @@
+import image1 from "./assests/syani1.JPG"
+import image2 from "./assests/Rebcca2.JPG"
+import image3 from "./assests/Riyan.JPG"
+import image4 from "./assests/Gargi4.JPG"
+import image5 from "./assests/Neeraj.JPG"
+import image6 from "./assests/Kaniska6.JPG"
+
+
+const data =[image1,image2,image3,image4,image5,image6 ]
+export default data;
