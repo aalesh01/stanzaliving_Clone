959e2aca94262eda25861e9125c4212edd30add5
aalesh01 <aalesh111999@gmail.com>
2022-06-29 11:06:00 +0530

    WA feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Homepage from './components/homepage/homepage';
+import WaBt from './components/waBT/waBT';
 
 function App() {
   return (
     <div className="App">
      <Homepage/>
+     <WaBt/>
     </div>
   );
 }
